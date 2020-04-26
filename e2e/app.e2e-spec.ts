0d78d88e9585a7d88b96e22e70cfb952dbb32d8d
sassi <sassi.hatem@gmail.com>
2020-04-26 15:48:26 +0200

    frontend app gestion stock

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { ClientGestionStockProduitsPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('client-gestion-stock-produits App', function() {
-  let page: ClientGestionStockProduitsPage;
+describe('client-gestion-stock-produits App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new ClientGestionStockProduitsPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
+import { HomeComponent } from './home/home.component';
+import { XhrInterceptor } from './authentication/xhr.interceptor';
+import { principalReducer } from './authentication/shared/principal.reducer';
+import { AuthenticationModule } from './authentication/authentication.module';
+import { DashboardModule} from './dashboard/dashboard.module';
+import { AppMenuModule } from './menu/app.menu.module';
+import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AuthenticationModule,
+    StoreModule.forRoot({principal: principalReducer}),
+    DashboardModule,
+    AppMenuModule,
+    ProductModule,
+    UserModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: XhrInterceptor, multi: true},
+    CookieService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
